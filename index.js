511a6283127fa44d7f7132ee54efd458dc6bf30a
lordson-silver <lordsonochuko5@gmail.com>
2022-09-15 00:37:07 +0100

    Added Product page and product details page and updated previous pages

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     MenuItemsEl.classList.remove("active");
 }))
+
+// --------- js for product gallery ------------
+var ProductImg = document.getElementById("product-img");
+var SmallImg = document.getElementsByClassName("small-img");
+
+
+for (let i = 0; i < SmallImg.length; i++) {
+    SmallImg[i].onclick = () => {
+    ProductImg.src = SmallImg[i].src;
+    };
+};
